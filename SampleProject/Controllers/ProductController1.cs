c3839dfa9f3f473a3869343d154d34b71499320e
alperen güneş <Alperen GNE>
2024-05-03 12:35:27 +0300

    ViewResult Action

---
+++ SampleProject/Controllers/ProductController1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SampleProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        #region ViewResult
+        public ViewResult GetProducts()
+        {
+            ViewResult result = View();
+            return result;
+
+        }
+        #endregion
+        #region PartialViewResult
+
+        #endregion
+        #region JsonResult
+
+        #endregion
+        #region EmptyResult
+
+        #endregion
+        #region ContentResult
+        #endregion
+        #region ActionResult
+        #endregion
+        #region IActionResult
+
+        #endregion
+    }
+}
---
+++ SampleProject/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace SampleProject.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+    }
+}
