542a4f6b96058993277bca4129bc22a35f50c192
alperen güneş <Alperen GNE>
2024-05-05 17:22:35 +0300

    Html helpers with 'Partial Method'

---
+++ UrIHelpers_HtmlHelpers_TagHelpers/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UrIHelpers_HtmlHelpers_TagHelpers.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ UrIHelpers_HtmlHelpers_TagHelpers/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UrIHelpers_HtmlHelpers_TagHelpers.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult GetProducts()
+        {
+            ViewBag.Message = "Hello World";
+            return View();
+        }
+    }
+}
