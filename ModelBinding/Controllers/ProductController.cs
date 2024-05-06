872cd039f95724c22a57f2aa1e1d785975a63070
alperen güneş <Alperen GNE>
2024-05-06 19:35:30 +0300

    Get data from form with 'IformCollection'

--- ModelBinding/Controllers/ProductController.cs
+++ ModelBinding/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ModelBinding.Models;
 
@@ -10,19 +11,32 @@ namespace ModelBinding.Controllers
         {
             return View();
         }
+        //public IActionResult CreateProduct()
+        //{
+        //    var product = new Product()
+        //    {
+        //        ProductName = "Z Product",
+        //        Quantity = 22 
+        //    };
+        //    return View(product);
+        //}
+        //[HttpPost]
+        ////public IActionResult CreateProduct(string txtProductName,string txtQuantity)
+        //public IActionResult CreateProduct(Product product)
+        //{
+        //    return View();
+        //}
         public IActionResult CreateProduct()
         {
-            var product = new Product()
-            {
-                ProductName = "Z Product",
-                Quantity = 22 
-            };
-            return View(product);
+            return View();
         }
         [HttpPost]
-        //public IActionResult CreateProduct(string txtProductName,string txtQuantity)
-        public IActionResult CreateProduct(Product product)
+        public IActionResult CatchData(IFormCollection  datas)
         {
+            var value1 = datas["txtValue1"].ToString();
+            var value2 = datas["txtValue2"];
+            var value3 = datas["txtValue3"];
+
             return View();
         }
 
