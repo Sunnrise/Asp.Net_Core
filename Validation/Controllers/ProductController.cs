c9da00e95cbad1afa3c34ccc0ca3540656aa3ca7
alperen güneş <Alperen GNE>
2024-05-10 20:37:26 +0300

    asp-validation-for with taghelpers

--- Validation/Controllers/ProductController.cs
+++ Validation/Controllers/ProductController.cs
@@ -14,18 +14,34 @@ namespace Validation.Controllers
         {
             return View();
         }
+        #region ModelState
 
-        [HttpPost]  
+        //[HttpPost]
+        //public IActionResult CreateProduct(Product model)
+        //{
+        //    //ModelState is a property of the Controller which can check a types data annotations and return a boolean value
+        //    if (!ModelState.IsValid)
+        //    {
+        //        //Loggin the error
+        //        ViewBag.Errmsg = ModelState.Values.FirstOrDefault(x => x.ValidationState == Microsoft.AspNetCore.Mvc.ModelBinding.ModelValidationState.Invalid).Errors[2].ErrorMessage;
+
+        //        return null;
+
+
+        //    }
+        #endregion
+        #region Modelstate with ValidationMessageTagHelper
+        [HttpPost]
         public IActionResult CreateProduct(Product model)
         {
             //ModelState is a property of the Controller which can check a types data annotations and return a boolean value
             if (!ModelState.IsValid)
             {
                 //Loggin the error
-                ViewBag.Errmsg = ModelState.Values.FirstOrDefault(x => x.ValidationState== Microsoft.AspNetCore.Mvc.ModelBinding.ModelValidationState.Invalid).Errors[2].ErrorMessage;
+                var messages = ModelState.ToList();
 
-                return null;
-              
+                return View(model);
+
 
             }
             //If the model is valid, the user will be redirected to the GetProducts view
@@ -34,6 +50,8 @@ namespace Validation.Controllers
 
 
             return View();
+            #endregion
+
         }
     } 
 }
