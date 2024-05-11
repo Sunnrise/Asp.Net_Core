a28b34c117ae59e1df41918f6f299a520ced804a
alperen güneş <Alperen GNE>
2024-05-11 21:23:11 +0300

    'RenderBody' and 'RenderSection' with _Layout

---
+++ Layout/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Layout.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Page1()
+        {
+            return View();
+        }
+        public IActionResult Page2()
+        {
+            return View();
+        }
+
+    }
+
+}
