55b43caad802e2e61336449537afef2805aa97dd
alperen güneş <Alperen GNE>
2024-05-13 01:43:03 +0300

    ViewModel with more than one object

---
+++ ViewModel_DTO/Models/ViewModels/ObjectViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ViewModel_DTO.Models.ViewModels
+{
+    public class ObjectViewModel
+    {
+        public Employee Employee { get; set; }
+        public Customer Customer { get; set; }
+        public Product Product { get; set; }
+    }
+}
