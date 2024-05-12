f61446c6c36a12ef3f71590e2bfd102832b1a03a
alperen güneş <Alperen GNE>
2024-05-12 22:09:00 +0300

    Areas Creation

---
+++ Areas/Areas/Admin_Panel/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Areas.Areas.Admin_Panel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
