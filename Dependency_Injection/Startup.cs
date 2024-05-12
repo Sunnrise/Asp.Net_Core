6049db4e44e87d9fcae394779935c7a41c8f8898
alperen güneş <Alperen GNE>
2024-05-12 21:05:56 +0300

    Scoped Dependency Injection

--- Dependency_Injection/Startup.cs
+++ Dependency_Injection/Startup.cs
@@ -32,6 +32,11 @@ namespace Dependency_Injection
             //services.AddSingleton<ConsoleLog>();// ConsoleLog's consturctor has not a parameter so we can use this method
             services.AddSingleton<ConsoleLog>(p=> new ConsoleLog(5));// ConsoleLog's consturctor has a parameter so we can use this method
             services.AddSingleton<TextLog>();
+
+            services.AddScoped<ConsoleLog>(p => new ConsoleLog(5));
+            services.AddScoped<TextLog>();
+
+
             services.AddControllersWithViews();
 
         }
