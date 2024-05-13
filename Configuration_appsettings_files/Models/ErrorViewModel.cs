6fac63c2b46f97dac733c0b789b25c06f6b4c9b5
alperen güneş <Alperen GNE>
2024-05-13 17:32:20 +0300

    Configure custom appsettings.json file

---
+++ Configuration_appsettings_files/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Configuration_appsettings_files.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
