1f1e824d7ec033419cfbc8b9931662d8fcd97fdf
alperen güneş <Alperen GNE>
2024-05-13 19:55:52 +0300

    secret.json handlings

---
+++ SecretManagerTools/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Data.SqlClient;
+
+//C:\Users\{username}\AppData\Roaming\Microsoft\UserSecrets\{userSecretsId}\secrets.json
+namespace SecretManagerTools.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        readonly IConfiguration configuration;
+        public HomeController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public void Index()
+        {
+            var user=configuration["MailInfo:User"];
+            var password = configuration["MailInfo:Password"];
+
+            var connection = configuration.GetConnectionString("SQL");
+            SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder(connection);
+            sqlConnectionStringBuilder.UserID = configuration["SQL:UserName"];
+            sqlConnectionStringBuilder.Password = configuration["SQL:Password"];
+            connection = sqlConnectionStringBuilder.ConnectionString;
+            Console.WriteLine();
+
+
+        }
+    }
+}
